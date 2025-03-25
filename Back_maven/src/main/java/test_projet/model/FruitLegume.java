152d9ecfc0fafa4a6fa230e6f4e66a718b923d2d
souhaila-el <souhaila.elhammouchi@gmail.com>
2025-03-25 14:51:04 +0100

    jpa sur plantes et ses classes filles

--- Back_maven/src/main/java/test_projet/model/FruitLegume.java
+++ Back_maven/src/main/java/test_projet/model/FruitLegume.java
@@ -1,8 +1,15 @@
 package test_projet.model;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("fruits_legumes")
 public class FruitLegume extends Plante{
 
-	
+	public FruitLegume() {
+		// TODO Auto-generated constructor stub
+	}
 
 	public FruitLegume(String nom, String description, int delaiRecolte, int dureeVie, int delaiArrosage) {
 		super(nom, description, delaiRecolte, dureeVie, delaiArrosage);
@@ -10,7 +17,7 @@ public class FruitLegume extends Plante{
 
 	@Override
 	public String toString() {
-		return "FruitLegume [nom=" + nom + ", description=" + description + ", datePlante=" + datePlante
+		return "FruitLegume [nom=" + nom + ", description=" + description
 				+ ", delaiRecolte=" + delaiRecolte + ", dureeVie=" + dureeVie + ", delaiArrosage=" + delaiArrosage
 				+ ", cultures=" + cultures + "]";
 	}
