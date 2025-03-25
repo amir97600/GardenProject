972c8e68536de94256cef840a177281eaa358f10
antoine <antoinef0312@gmail.com>
2025-03-25 14:57:04 +0100

    MapJardin

--- Back_maven/src/main/java/test_projet/model/Jardin.java
+++ Back_maven/src/main/java/test_projet/model/Jardin.java
@@ -3,15 +3,38 @@ package test_projet.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="jardin")
 public class Jardin {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="numero",nullable=false)
 	private int numero;
+	
+	@Column(name="nom", nullable=false)
 	private String nom;
+	
+	@Column(name="lieu", nullable=false)
 	private String lieu;
-	private int superficie;
+	
+	@Column(name="superficie", nullable=false, columnDefinition = "DECIMAL(8,2)")
+	private double superficie;
+	
+	@OneToMany(mappedBy = "jardin")
 	private List<Culture> cultures = new ArrayList<Culture>();
 
-	public Jardin(int numero, String nom, String lieu, int superficie) {
+	public Jardin() {}
+	
+	public Jardin(int numero, String nom, String lieu, double superficie) {
 		this.numero = numero;
 		this.nom = nom;
 		this.lieu = lieu;
@@ -62,14 +85,14 @@ public class Jardin {
 
 
 
-	public int getSuperficie() {
+	public double getSuperficie() {
 		return superficie;
 	}
 
 
 
 
-	public void setSuperficie(int superficie) {
+	public void setSuperficie(double superficie) {
 		this.superficie = superficie;
 	}
 
