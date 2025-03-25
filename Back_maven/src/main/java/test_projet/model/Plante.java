5658b88c8c1fe6555f5291c290172e2a409cd2eb
amir97600 <amir97600@github.com>
2025-03-25 16:48:08 +0100

    correction d'erreurs jpa

--- Back_maven/src/main/java/test_projet/model/Plante.java
+++ Back_maven/src/main/java/test_projet/model/Plante.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -15,6 +16,7 @@ import javax.persistence.Table;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "type_plante",columnDefinition = "ENUM('fleurs','fruits_legumes')")
 @Table(name="plante")
 public abstract class Plante {
 	
