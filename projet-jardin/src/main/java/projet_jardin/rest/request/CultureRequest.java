0551f49d0fadd797fd8d3aaf03b51fdc9852e8f1
Marina Aubry <aubry.marina20@gmail.com>
2025-04-16 13:24:53 +0200

    Fix CultureRequest.convert()

--- projet-jardin/src/main/java/projet_jardin/rest/request/CultureRequest.java
+++ projet-jardin/src/main/java/projet_jardin/rest/request/CultureRequest.java
@@ -3,20 +3,15 @@ package projet_jardin.rest.request;
 import java.time.LocalDate;
 
 import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
 
-import projet_jardin.dao.IDAOJardin;
-import projet_jardin.dao.IDAOPlante;
 import projet_jardin.model.Culture;
+import projet_jardin.model.Fleur;
+import projet_jardin.model.FruitLegume;
+import projet_jardin.model.Jardin;
+import projet_jardin.model.Plante;
 
 
 public class CultureRequest {
-	
-
-	private static IDAOJardin daoJardin;
-	private static IDAOPlante daoPlante;
 	
 	private Integer id;
 	private int quantite;
@@ -25,6 +20,7 @@ public class CultureRequest {
 	private Boolean recolte;
 	private Integer idJardin;
 	private Integer idPlante;
+	private PlanteType planteType;
 	
 	public CultureRequest () {}
 
@@ -84,14 +80,38 @@ public class CultureRequest {
 		this.idPlante = idPlante;
 	}
 	
+	
+	public PlanteType getPlanteType() {
+		return planteType;
+	}
+
+	public void setPlanteType(PlanteType planteType) {
+		this.planteType = planteType;
+	}
+
 	public static Culture convert(CultureRequest cultureRequest) {
 		Culture culture = new Culture();
 		
 		BeanUtils.copyProperties(cultureRequest, culture);
-		culture.setJardin(daoJardin.findById(cultureRequest.getIdJardin()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND)));
-		culture.setPlante(daoPlante.findById(cultureRequest.getIdPlante()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND)));
+		
+		Jardin jardin= new Jardin();
+		jardin.setNumero(cultureRequest.getIdJardin());
+		
+		Plante plante = null;
+		if(cultureRequest.getPlanteType() == PlanteType.Fleur) {
+			plante = new Fleur();
+		} else if(cultureRequest.getPlanteType() == PlanteType.FruitLegume) {
+			plante = new FruitLegume();
+		} 
+		plante.setId(cultureRequest.getIdPlante());
+		culture.setJardin(jardin);
+		culture.setPlante(plante);
 		
 		return culture;
+	}
+	
+	public enum PlanteType {
+		FruitLegume, Fleur;
 	}
 
 }
