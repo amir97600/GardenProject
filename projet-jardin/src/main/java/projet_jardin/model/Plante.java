605664bb8edd3741c6c1e31a92c7ced98337cac8
souhaila-el <souhaila.elhammouchi@gmail.com>
2025-04-16 13:00:45 +0200

    getters/setters id

--- projet-jardin/src/main/java/projet_jardin/model/Plante.java
+++ projet-jardin/src/main/java/projet_jardin/model/Plante.java
@@ -50,6 +50,14 @@ public abstract class Plante {
 	}
 	
 	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
 	public String getNom() {
 		return nom;
 	}
