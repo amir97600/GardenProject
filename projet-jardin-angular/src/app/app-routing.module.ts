277677f4884f1ac0fc918c3c65d6f3f41fdbf355
antoine <antoinef0312@gmail.com>
2025-04-25 14:16:34 +0200

    add NavBar

--- projet-jardin-angular/src/app/app-routing.module.ts
+++ projet-jardin-angular/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path : 'home'},
+  // {path : 'cultures'},
+  // {path : 'profil'},
+  // {path : 'explorer'},
+  // {path : 'deconnecter'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projet-jardin-angular/src/app/app.module.ts
+++ projet-jardin-angular/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LucideAngularModule, Home, Leaf, User, Search, LogOut } from 'lucide-angular';
+import { GardenNavigationComponent } from './garden-navigation/garden-navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GardenNavigationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LucideAngularModule.pick({Home, Leaf, User, Search, LogOut})
   ],
   providers: [],
   bootstrap: [AppComponent]
