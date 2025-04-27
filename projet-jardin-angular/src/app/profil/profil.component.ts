dea407f8565ffd76a776fb6782be972e90d95108
Marina Aubry <aubry.marina20@gmail.com>
2025-04-27 16:55:04 +0200

    Fix initialization Client & Jardin in ProfilComponent

--- projet-jardin-angular/src/app/profil/profil.component.ts
+++ projet-jardin-angular/src/app/profil/profil.component.ts
@@ -16,14 +16,12 @@ import { jwtDecode } from 'jwt-decode';
   styleUrl: './profil.component.css'
 })
 export class ProfilComponent {
-  client!: Client;
-  jardin!: Jardin;
+  client: Client = new Client("","","","",0);
+  jardin: Jardin = new Jardin("",0);
   // Liste de tous les badges 
   badges = Object.entries(Badge).filter(([key, value]) => typeof value === 'number');
   // Liste des badges débloqués par le client en fonction de son score 
   badgesDebloques: string[] = [];
-  
-
 
   constructor(private router : Router, private clientService : ClientService, private jardinService : JardinService) {}
 
