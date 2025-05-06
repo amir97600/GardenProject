ba762ffc070cf3d4af0fc84f4910118bc90834b5
amirdine <amir974320@gmail.com>
2025-05-06 10:38:32 +0200

    remove environment.ts en trop et controllerRestVille

--- projet-jardin/src/main/java/projet_jardin/rest/CommonRestController.java
+++ projet-jardin/src/main/java/projet_jardin/rest/CommonRestController.java
@@ -3,21 +3,16 @@ package projet_jardin.rest;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import projet_jardin.config.jwt.JwtUtil;
-import projet_jardin.dao.IDAOUtilisateur;
 import projet_jardin.rest.request.ConnexionRequest;
 import projet_jardin.rest.response.ConnexionResponse;
 
@@ -27,8 +22,6 @@ public class CommonRestController {
 	
 	@Autowired
 	private AuthenticationManager authenticationManager;
-
-	private final RestTemplate restTemplate = new RestTemplate();
 
 	public CommonRestController() {
 		super();
@@ -71,18 +64,5 @@ public class CommonRestController {
 		return connexionResponse;
 	}
 
-
-    @GetMapping("/ville/{codePostal}")
-    public ResponseEntity<?> getVilleParCodePostal(@PathVariable String codePostal) {
-        
-		String apiUrl = "https://api.zippopotam.us/fr/" + codePostal;
-
-        try {
-            ResponseEntity<String> response = restTemplate.getForEntity(apiUrl, String.class);
-            return ResponseEntity.ok(response.getBody());
-        } catch (Exception e) {
-            return ResponseEntity.status(404).body("Ville introuvable ou erreur de l'API");
-        }
-    }
     
 }
