8d0c2251d2f7ce3626cbb14b61db814d3b6c9358
antoine <antoinef0312@gmail.com>
2025-05-15 10:39:55 +0200

    Pluie

--- projet-jardin-angular/src/app/environment/environment.ts
+++ projet-jardin-angular/src/app/environment/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     apiUrl: 'http://localhost:8080/api',
     apiVille: 'https://geo.api.gouv.fr/communes?codePostal=',
-    meteoBaseUrl: 'https://public-api.meteofrance.fr/public/DPObs/v1/station/infrahoraire-6m'
+    meteoBaseUrl: 'https://public-api.meteofrance.fr/public/DPObs/v1/station/infrahoraire-6m',
+    arrossage: 'https://public-api.meteofrance.fr/public/DPObs/v1/station/horaire'
 };
